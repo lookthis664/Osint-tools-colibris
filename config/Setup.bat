77af111a7f81492e9e0e274a8f26e0c0f90551b6
I2S_372 <103644235+lookthis664@users.noreply.github.com>
2023-09-20 20:27:10 +0200

    Add files via upload

---
+++ config/Setup.bat
@@ -0,0 +1,26 @@
+@echo off
+:start
+cls
+
+set python_ver=3.11
+
+python ./get-pip.py
+
+cd \
+cd \python%python_ver%\Scripts\
+pip install time
+pip install os
+pip install colorama
+pip install pystyle
+pip install dhooks
+pip install ctypes
+pip install requests
+pip install discord_webhook
+pip install json
+pip install random
+pip install argparse
+pip install selenium
+pip install datetime
+
+pause
+exit
---
+++ pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.3.7 (trial), 000000, 2023-09-20T20:21:30.022884
+from .pyarmor_runtime import __pyarmor__
