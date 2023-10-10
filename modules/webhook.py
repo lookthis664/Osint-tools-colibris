b2b6e87bf7f9b20e5a88d94816f982d49bae7fe2
I2S_372 <103644235+lookthis664@users.noreply.github.com>
2023-10-10 21:02:14 +0200

    Add files via upload

---
+++ modules/webhook.py
@@ -0,0 +1,16 @@
+import os 
+from discord_webhook import DiscordWebhook
+import requests
+from colorama import Fore 
+
+async def change_the_webhook(wbh):
+    os.system('cls' or 'clear')
+    with open(r"config\src\webhook.txt", "w") as file:
+       file.write(wbh)
+
+    r = requests.post(wbh)
+    if r.status_code != 404:
+           webhook = DiscordWebhook(url=wbh, content="Launch the webhook")
+           response = webhook.execute()
+    else:
+       print(f'{Fore.RED}[?]=> Webhook isn\'t working: {r.text}{Fore.RESET}')
