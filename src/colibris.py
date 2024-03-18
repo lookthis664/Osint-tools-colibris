4f2a4fcabfdad6e6dbc404408dfb5fd6ae30dde6
372._ <103644235+lookthis664@users.noreply.github.com>
2024-03-18 21:57:05 +0100

    Update colibris.py

--- src/colibris.py
+++ src/colibris.py
@@ -12,6 +12,7 @@ def main():
     descriptions = loop.run_until_complete(banners.menu_help())
     parser = argparse.ArgumentParser(description=descriptions)
     parser.add_argument('-u', '--username', help="it gives you all the links attached to the username")
+    parser.add_argument('-g', '--github', help="it gives you the email + name + GitHub username of a person")
     parser.add_argument('-w', '--webhook', help="Change your webhook")
     args = parser.parse_args()
 
@@ -22,11 +23,19 @@ def main():
         loop.run_until_complete(username.username_search(usernames))
         loop.run_until_complete(gs.gs_search(usernames))
         loop.run_until_complete(apigithub.username_search(usernames))
+        
+    elif args.github:
+        os.system('cls' or 'clear')
+        usernames = args.github
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(apigithub.username_search(usernames))
 
     elif args.webhook:
+        os.system('cls' or 'clear')
         wbh = args.webhook
         loop = asyncio.get_event_loop()
         loop.run_until_complete(webhook.change_the_webhook(wbh))
+
     else:
         print("No arguments provided.")
 
