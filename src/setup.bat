079d7e266f8bb444af108fdc447b08f6bde6fff4
372._ <103644235+lookthis664@users.noreply.github.com>
2025-11-25 18:03:42 +0100

    Add files via upload

---
+++ src/setup.bat
@@ -0,0 +1,16 @@
+@echo off
+
+
+:start
+cls
+
+set python_ver=3.11
+python ./get-pip.py
+
+cd \
+cd \python%python_ver%\Scripts\
+python -m pip install colorama playwright
+python -m playwright install
+
+pause
+exit
