8b5d07624ff7ce4c5170556bbcb1f6d9f8dfeeb0
372._ <103644235+lookthis664@users.noreply.github.com>
2025-11-25 20:52:28 +0100

    Update setup.bat

--- src/setup.bat
+++ src/setup.bat
@@ -9,7 +9,7 @@ python ./get-pip.py
 
 cd \
 cd \python%python_ver%\Scripts\
-python -m pip install colorama playwright BeautifulSoup requests
+python -m pip install colorama playwright BeautifulSoup requests bs4
 python -m playwright install
 
 
